a3d30a1c2106ce87218487da0875c43b147d271e
Michel Albert <michel@albert.lu>
2010-06-12 14:49:14 +0200

    Fixed turbogears install command

--- install.sh
+++ install.sh
@@ -6,7 +6,8 @@ fi
 
 source ./venv/bin/activate || exit 1
 
-easy_install turbogears
+easy_install -i http://www.turbogears.org/2.0/downloads/current/index tg.devtools
 easy_install sqlalchemy
+easy_install sqlalchemy-migrate
 easy_install psycopg2
 
